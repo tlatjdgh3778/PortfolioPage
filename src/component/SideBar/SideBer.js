77c627bae95d4797ef6b3fe97a36cd6aa2843aaa
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-04-11 18:27:53 +0900

    width 크기 수정

--- src/component/SideBar/SideBer.js
+++ src/component/SideBar/SideBer.js
@@ -15,7 +15,7 @@ border: 2px solid ${(props) => props.theme.color.fontColor};
 background-color: ${(props) => props.theme.color.bgColor};
 height: 100%;
 border-radius: 30px;
-width: 20%;
+width: 21%;
 box-shadow: 0 0 10px ${(props) => props.theme.color.highlightColor};
 
     @media ${(props) => props.theme.device.TabletLandscape} {
