9e025e05d2ba2e8285b028969069800b49f2e07d
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-19 20:08:50 +0900

    렌더링 최적화 작업

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -3,27 +3,43 @@ import projectData from 'data/projectData';
 import { ProjectCard } from 'components/Project/index';
 import * as S from './Project.style';
 
+const initialState = projectData;
+
 const Project = () => {
-    const [projects, setProjects] = useState(projectData);
-    console.log(projectData);
+    console.log('Project Render');
+    const [projects, setProjects] = useState(initialState);
+    const [filter, setFilter] = useState('All');
 
     const clickFilter = (name) => {
-        const new_array = projects.filter(project => project.category.includes(name));
-        setProjects(new_array);
-    }
+        if(filter !== name){
+            if(name === 'All'){
+                setProjects(initialState);
+            }else{
+                // filter 함수로 배열을 새로운 배열을 만들면 리렌더링 할 때,
+                // 배열 안의 내용이 동일해도 false이므로 렌더링을 한다.
+                // => filter state 를 둬서 클릭한(React, VanilaJS ...) 값(name)과 그 전의 filter가 
+                // 다르면 project 목록 출력.
+                // 같으면 렌더링 하지 않는다.
+                setProjects(initialState.filter(project => project.category.includes(name)));
+            }
+        setFilter(name);
+        }
+    };
+    // const memoizeFilter = useMemo(() => clickFilter(name), []);
+
     return(
 
         <S.ProjectStyle>
             <S.ProjectFilter>
                 <S.FilterBtn 
-                onClick={() => {setProjects(projectData);}}>All</S.FilterBtn>
+                onClick={e => clickFilter(e.target.childNodes[0].data)}>All</S.FilterBtn>
                 <S.FilterBtn 
-                onClick={() => clickFilter('React')}>React</S.FilterBtn>
+                onClick={e => clickFilter(e.target.childNodes[0].data)}>React</S.FilterBtn>
                 <S.FilterBtn 
-                onClick={() => clickFilter('VanilaJS')}>VanilaJS</S.FilterBtn>
+                onClick={e => clickFilter(e.target.childNodes[0].data)}>VanilaJS</S.FilterBtn>
             </S.ProjectFilter>
                 <S.ProjectList>
-                    {projects.map((project, i)=>{
+                    {projects.map((project)=>{
                         return <ProjectCard key={project.name} project={project}></ProjectCard>
                     })}
                 </S.ProjectList>
