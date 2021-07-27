1705fc0f30a8b422373ba674f933387f743bfd63
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-27 18:05:46 +0900

    update Introduce

--- src/components/About/AboutIntroduce/AboutIntroduce.jsx
+++ src/components/About/AboutIntroduce/AboutIntroduce.jsx
@@ -9,7 +9,7 @@ const AboutIntroduce = () => {
             <GS.AboutContentTitle>Introduce</GS.AboutContentTitle>
             <GS.AboutContent>
                 <S.Introduce>
-                    실력 있고 인정받는 프론트 엔드 개발자를 꿈꾸고 있는 신입 개발자입니다. React 와 Styled Components 사용하여 깔끔한 코드를 작성하기 위해 노력하고 있습니다.
+                    실력 있고 인정받는 프론트 엔드 개발자를 꿈꾸고 있는 신입 개발자입니다. React 와 react-redux 를 주로 사용하고 스타일링은 Styled-component 와 Material-UI를 사용하고 있습니다. 새로운 기술에 대해 알게 되는 것을 즐겁게 생각합니다.
                 </S.Introduce>
             </GS.AboutContent>
         </GS.AboutContentContainer>
